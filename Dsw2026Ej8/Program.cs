09af420077d7c0b5c824edaa5c7328aaf52eca70
Zambrano Agustina Nahir <aguszambrano01@gmail.com>
2026-05-03 05:00:28 -0300

    add Problema 1: clase parcial

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Problema1;
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var productHelper = new ProductHelper();
+            string etiqueta = productHelper.ObtenerEtiquetaProducto(12345, "Camiseta", 19.99m);
+            Console.WriteLine(etiqueta);
         }
     }
 }
