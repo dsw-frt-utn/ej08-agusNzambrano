babfe2c89a8a5dcc29df27ad4caf46791c9ff328
Zambrano Agustina Nahir <aguszambrano01@gmail.com>
2026-05-03 05:53:23 -0300

    add Problema 2: Anonymous Class

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -7,7 +7,14 @@ namespace Dsw2026Ej8
         {
             var productHelper = new ProductHelper();
             string etiqueta = productHelper.ObtenerEtiquetaProducto(12345, "Camiseta", 19.99m);
+            Console.WriteLine("--Prueba Ej01--");
             Console.WriteLine(etiqueta);
+
+            var p2 = new Dsw2026Ej8.Problema2();
+            string resumen = p2.CrearResumenVenta(12345, "Camiseta", 2, 19.99m);
+            Console.WriteLine("--Prueba Ej02--");
+            Console.WriteLine(resumen);
+
         }
     }
 }
